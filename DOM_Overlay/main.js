3f242d8c795bf61b9266b0861d57b3cc75b3ae24
Rene Mühlböck <20172023@atn.ac.at>
2021-06-25 09:51:14 +0200

    testing source of DOM overlays

--- DOM_Overlay/main.js
+++ DOM_Overlay/main.js
@@ -28,7 +28,7 @@ async function activateXR() {
     camera.matrixAutoUpdate = false;
 
     //Initialize WebXR session using "immersive-ar"
-    const session = await navigator.xr.requestSession("immersive-ar", { requiredFeatures: ['hit-test'], optionalFeatures: ['dom-overlay'], domOverlay: { root: document.body } });
+    const session = await navigator.xr.requestSession("immersive-ar", { requiredFeatures: ['hit-test'], optionalFeatures: ['dom-overlay'], domOverlay: { root: document.getElementById('ar-button') } });
     session.updateRenderState({
         baseLayer: new XRWebGLLayer(session, gl)
     });
