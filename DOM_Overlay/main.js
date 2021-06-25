5c7a88ef162b4548c0de82399911ddbc2ab2f915
MayMax123 <max.mayrhofer03@gmail.com>
2021-06-25 11:31:07 +0200

    sasfafd

--- DOM_Overlay/main.js
+++ DOM_Overlay/main.js
@@ -37,7 +37,6 @@ async function activateXR() {
 
     if (session) {
         divButtons.style.display = 'block';
-        divButtons.style.visibility = 'hidden';
         document.getElementById('session-info').innerHTML = "DOM Overlay type: " + session.domOverlayState.type;
     }
 
