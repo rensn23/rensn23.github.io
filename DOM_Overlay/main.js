ab7f03fd86e7670e2ec8979e99c4d631a42e5cef
Rene Mühlböck <20172023@atn.ac.at>
2021-06-25 08:41:44 +0200

    added files for DOM testing

---
+++ DOM_Overlay/main.js
@@ -0,0 +1,67 @@
+async function activateXR() {
+    const canvas = document.createElement("canvas");
+    document.body.appendChild(canvas);
+    const gl = canvas.getContext("webgl", { xrCompatible: true });
+
+    //Three.js initialization
+    const scene = new THREE.Scene();
+
+    //Axes Helper
+    const axesHelper = new THREE.AxesHelper(20);
+    scene.add(axesHelper);
+
+    // WebGLRenderer
+    const renderer = new THREE.WebGLRenderer({
+        alpha: true,
+        preserveDrawingBuffer: true,
+        canvas: canvas,
+        context: gl
+    });
+    renderer.autoClear = false;
+    renderer.localClippingEnabled = true;
+
+    //Disable matrix auto updates from three.js
+    const camera = new THREE.PerspectiveCamera();
+    camera.matrixAutoUpdate = false;
+
+    //Initialize WebXR session using "immersive-ar"
+    const session = await navigator.xr.requestSession("immersive-ar", { requiredFeatures: ['hit-test'] });
+    session.updateRenderState({
+        baseLayer: new XRWebGLLayer(session, gl)
+    });
+
+    //Create a position reference near the user
+    const referenceSpace = await session.requestReferenceSpace('local');
+    const viewerSpace = await session.requestReferenceSpace('viewer');
+
+    //Hit testing with user as the origin
+    const hitTestSource = await session.requestHitTestSource({ space: viewerSpace });
+
+    //Render loop for drawing in AR
+    const onXRFrame = (time, frame) => {
+        //Next draw request
+        session.requestAnimationFrame(onXRFrame);
+
+        //Bind graphics framebuffer to baseLayer framebuffer
+        gl.bindFramebuffer(gl.FRAMEBUFFER, session.renderState.baseLayer.framebuffer);
+
+        //Retrieve position of the device WARNING: "XRFrame.getViewerPose" can return null when tracking is tried to be first established
+        const pose = frame.getViewerPose(referenceSpace);
+
+        if (pose !== null) {
+            const view = pose.views[0];
+
+            const viewport = session.renderState.baseLayer.getViewport(view);
+            renderer.setSize(viewport.width, viewport.height)
+
+            //Use the view's transform matrix and projection matrix to configure the THREE.camera
+            camera.matrix.fromArray(view.transform.matrix);
+            camera.projectionMatrix.fromArray(view.projectionMatrix);
+            camera.updateMatrixWorld(true);
+
+            //Render the scene
+            renderer.render(scene, camera);
+        }
+    }
+    session.requestAnimationFrame(onXRFrame);
+}
